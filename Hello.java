f35559c34232031f7a104b13902942db142a5941
Sanjay <sejadhav@gmail.com>
2017-09-11 00:19:14 -0500

    added iterations

--- Hello.java
+++ Hello.java
@@ -1,12 +1,12 @@
 
 // Adding comments 
 // Added by Sanjay
-//
-public class Hello.java
+// Added 20 iterations
+public class Hello
 {
- public void static main(String args){
+ public static void main(String[] args){
      
-	 for (int i=0;i<10;i++){
+	 for (int i=1;<20;i++){
 	 
 	 System.out.println("Hello Sanjay : "+i);
 	 }
